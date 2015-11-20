8f2cc4a8137226dd37c53c352abf59b0ed95443f
tim <therrma2@gmail.com>
2015-11-19 18:41:28 -0600

    Started collision check psuedocode

---
+++ collision.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import cv
+import cv2
+import numpy
+import sys
+from math import pi
+
+im = Image.new("RGB",(700,700),(256,256,256))
+
+while theta 1 is 0:
+    compute each configuration in .01 increments of theta 1.
+    check for collision
+    if collision, mark pixel black
+
+
